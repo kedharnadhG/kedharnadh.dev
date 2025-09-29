6fa5e872f350b0196c565512c9a717fbe82b6bcb
kedharnadhG <g.kedharnadh098@gmail.com>
2025-09-29 10:37:56 +0530

    filename changed

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/general/button";
 const DownloadCV = () => {
   return (
     <Button
-      onClick={() => window?.open("/files/KedharNadh_2023.pdf", "_blank")}
+      onClick={() => window?.open("/files/Kedharnadh_2023.pdf", "_blank")}
     >
       Download CV
     </Button>
